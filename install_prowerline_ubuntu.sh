9423d2d25b9a285195671cd02f427035df6ad549
gawaboga <gawaboga@github.com>
2024-01-20 17:56:05 +0000

    pg scripts

---
+++ install_prowerline_ubuntu.sh
@@ -0,0 +1,13 @@
+# Fail on any command.
+set -eux pipefail
+sudo apt-get upgrade -y
+# Install Powerline for VIM.
+sudo apt-get install -y python3-pip
+pip3 install --user powerline-status
+sudo cp configs/.vimrc ~/.vimrc
+sudo apt-get  install -y fonts-powerline
+
+# Install Patched Font
+mkdir ~/.fonts
+sudo cp -a fonts/. ~/.fonts/
+fc-cache -vf ~/.fonts/
