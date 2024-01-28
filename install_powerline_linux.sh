767cfb503f7c278dd24be4a04d075f9907bcb25f
EC2 Default User <ec2-user@ip-172-31-41-243.ap-northeast-1.compute.internal>
2024-01-28 00:55:56 +0000

    updates

--- install_powerline_linux.sh
+++ install_powerline_linux.sh
@@ -6,11 +6,16 @@ sudo dnf install -y python3-pip
 sudo yum install git
 pip3 install --user powerline-status
 sudo cp configs/.vimrc ~/.vimrc
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
+# Create a temporary directory and clone Powerline fonts into it.
+temp_fonts_dir=$(mktemp -d)
+git clone https://github.com/powerline/fonts.git --depth=1 "$temp_fonts_dir/fonts"
+cd "$temp_fonts_dir/fonts"
 ./install.sh
-cd ..
-rm -rf fonts
+
+# Clean up: remove the temporary directory.
+cd ~
+rm -rf "$temp_fonts_dir"
+
 # Install Patched Font
 mkdir ~/.fonts
 sudo cp -a fonts/. ~/.fonts/
