0f7eda2bbb6585b767b9775cd49c7c86a50a67cb
EC2 Default User <ec2-user@ip-172-31-41-243.ap-northeast-1.compute.internal>
2024-01-28 00:50:08 +0000

    updates

--- install_powerline_linux.sh
+++ install_powerline_linux.sh
@@ -1,12 +1,16 @@
 # Fail on any command.
-set -eux pipefail
+set -euxo pipefail
 
 # Install Powerline for VIM.
 sudo dnf install -y python3-pip
+sudo yum install git
 pip3 install --user powerline-status
 sudo cp configs/.vimrc ~/.vimrc
-sudo dnf install -y fonts-powerline
-
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
 # Install Patched Font
 mkdir ~/.fonts
 sudo cp -a fonts/. ~/.fonts/
