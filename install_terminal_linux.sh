60e00aea1bdfb83251f2faa0deb47f03d7102461
EC2 Default User <ec2-user@ip-172-31-41-243.ap-northeast-1.compute.internal>
2024-01-28 01:05:27 +0000

    updates

--- install_terminal_linux.sh
+++ install_terminal_linux.sh
@@ -1,6 +1,6 @@
 # Fail on any command.
-set -eux pipefail
+set -euxo pipefail
 
 # Install ZSH
-sudo dnf install -y git-core zsh curl
+sudo dnf install -y git-core zsh curl --allowerasing
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
